c79333061df2be7adb36b2021bcc914a79924298
nguyenkhoaquan161004 <khoaquan312@gmail.com>
2025-05-29 14:34:57 +0700

    Platform.OS web - mobile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
 import { AuthProvider } from '@/contexts/AuthContent'; // hoặc AuthContext nếu đúng tên file bạn tạo
 import { Slot } from 'expo-router';
+import { Platform } from 'react-native';
+import W_StartScreen from './W_StartScreen';
 
-export default function RootLayout() {
-  return (
-    <AuthProvider>
-      <Slot />
-    </AuthProvider>
-  );
-}
+export default Platform.OS === 'web' ? W_StartScreen : (
+  function RootLayout() {
+    return (
+      <AuthProvider>
+        <Slot />
+      </AuthProvider>
+    );
+  }
+);
+
